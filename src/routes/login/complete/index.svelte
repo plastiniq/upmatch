d42d01f9c1b37299f4d3bd55dad158120a20e455
Dmitry Malyovaniy <plastiniq@gmail.com>
2019-10-24 20:22:33 +0300

    Add login redirect

--- src/routes/login/complete/index.svelte
+++ src/routes/login/complete/index.svelte
@@ -3,11 +3,10 @@
 
 	export async function preload({ params, query }) {
     const resp = await this.fetch(`/api/login/complete?${new URLSearchParams(query)}`, { credentials: 'include' })
-    console.log('complete', resp)
     if (resp.ok) {
-      // this.redirect(302, '/')
+      this.redirect(302, '/')
     } else {
-      // this.redirect(302, '/login')
+      this.redirect(302, '/login')
     }
 	}
 </script>
