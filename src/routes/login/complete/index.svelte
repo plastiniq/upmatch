07af14c7af82403ebf7e0b120f8810cbec81a853
Dmitry Malyovaniy <plastiniq@gmail.com>
2019-10-27 21:18:06 +0200

    Bug fix

--- src/routes/login/complete/index.svelte
+++ src/routes/login/complete/index.svelte
@@ -1,12 +1,19 @@
 <script context="module">
   import * as sapper from '@sapper/app';
 
+  let error
+
 	export async function preload({ params, query }) {
     const resp = await this.fetch(`/api/login/complete?${new URLSearchParams(query)}`, { credentials: 'include' })
     if (resp.ok) {
       this.redirect(302, '/')
     } else {
-      this.redirect(302, '/login')
+      error = resp
+      // this.redirect(302, '/login')
     }
 	}
 </script>
+
+<div>
+{error}
+</div>
